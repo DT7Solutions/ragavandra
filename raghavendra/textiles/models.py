7a3a75092ee7ae2de849a1647d97e480455200b5
website-krysta <website.krysta@gmail.com>
2023-04-25 13:09:41 +0530

    fixed all issues

--- raghavendra/textiles/models.py
+++ raghavendra/textiles/models.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.db import models
 
 # Create your models here.
@@ -9,14 +10,40 @@ class RegisterUsers(models.Model):
     FirstName = models.CharField(max_length=20, blank=False)
     LastName = models.CharField(max_length=20, blank=False)
     EmailID = models.EmailField()
-    Phone = models.CharField(max_length=10, unique=True)
+    Phone = models.IntegerField(max_length=10,)
     Password = models.CharField(max_length=25, blank=False)
     Address =  models.CharField(max_length=255,null=True, blank=True)
     City =  models.CharField(max_length=25,null=True, blank=True)
     State =  models.CharField(max_length=25,null=True, blank=True)
     Zip = models.CharField(max_length=10,default=0,null=True, blank=True)
     UserStatus = models.BooleanField(default=True)
-  
+
     def __int__(self):
             return self.UserID
+    
+class Orders(models.Model):
+    OrderID = models.AutoField(primary_key=True)
+    Name  = models.CharField(max_length=30,null=True, blank=True)
+    WhatsappNo = models.IntegerField(max_length=10,null=True, blank=True)
+    ContactNo = models.IntegerField(max_length=10,null=True, blank=True)
+    Address = models.CharField(max_length=250, blank=False)
+    street_name = models.CharField(max_length=255,null=True, blank=True)
+    city = models.CharField(max_length=255,null=True, blank=True)
+    state = models.CharField(max_length=255,null=True, blank=True)
+    postal_code = models.CharField(max_length=10,null=True, blank=True)
+    Courier = models.CharField(max_length=30, blank=False)
+    TrackingId = models.CharField(max_length=10,default=0,null=True, blank=True)
+    No_Of_Items = models.IntegerField(max_length=10,null=True, blank=True)
+    file = models.FileField(upload_to='media/')
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('approved', 'Approved'),
+        ('rejected', 'Rejected'),
+    )
+    OrderStatus = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    Date = models.DateTimeField(default=datetime.now())
+    UserID = models.ForeignKey('RegisterUsers', on_delete=models.CASCADE) 
+   
+    def __int__(self):
+            return self.OrderID
     
--- raghavendra/textiles/views.py
+++ raghavendra/textiles/views.py
@@ -1,21 +1,33 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .models import RegisterUsers
+from .models import RegisterUsers,Orders
 # Create your views here.
 def home(request):
+    if request.method=='POST':
+        phone = request.POST.get('phone',"")
+        password = request.POST.get('pass',"")
+        queryset = RegisterUsers.objects.all()
+        for fields in queryset:
+            if fields.Phone == int(phone) and fields.Password == password:
+                orders = Orders.objects.filter(UserID=fields.UserID)
+                userdata = RegisterUsers.objects.filter(UserID=fields.UserID)
+                return render(request, 'uifiles/orders.html', {'orders': orders ,'userdata':userdata})
+        
     return render(request ,"uifiles/home.html")
 
 def createAccount(request):
     if request.method=='POST':
-        
         fname = request.POST.get('fname',"")
         lname = request.POST.get('lname',"")
         email = request.POST.get('email',"")
-        Phone = request.POST.get('Phone',"")
+        phone = request.POST.get('Phone',"")
         password = request.POST.get('password',"")
-
-        oUser_info = RegisterUsers(FirstName=fname,LastName=lname,EmailID=email,Phone=Phone,Password=password)
-        oUser_info.save()
-        return HttpResponseRedirect('/login/')
-    
+        person_exists = RegisterUsers.objects.filter(Phone=phone).exists()
+        if person_exists:
+            return HttpResponseRedirect('/register/')
+        else:
+           oUser_info = RegisterUsers(FirstName=fname,LastName=lname,EmailID=email,Phone=phone,Password=password)
+           oUser_info.save()
+           return HttpResponseRedirect('/')
+       
     return render(request ,"uifiles/create-account.html")
